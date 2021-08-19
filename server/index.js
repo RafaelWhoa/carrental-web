eada1a24a5604fdafba2e793a3f41b74ee0fb9e6
Rafael Costa <rafaelwhoa@gmail.com>
2021-08-19 17:14:11 -0400

    Home page changes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const Mongoose = require('./config/db');
 const exphbs = require('express-handlebars');
 const flash = require('connect-flash');
 const session = require('express-session');
+require('../server/models/Agency')
+const Agency = mongoose.model('agency')
 
 //Session
 app.use(session({
@@ -37,7 +39,12 @@ app.use(express.static(__dirname + '/public'))
 //Routes
 app.use('/admin', admin)
 app.get('/', (req, res) => {
-    res.render('index')
+    Agency.find().lean().then((agency) => {
+        res.render('index', {agency:agency})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro interno')
+        res.redirect('/')
+    })
 })
 
 app.get('/500', (req, res) => {

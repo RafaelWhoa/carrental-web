f4a620706db42aadc6e129785e71d63e7a3eb2db
Rafael Costa <rafaelwhoa@gmail.com>
2021-08-18 20:37:25 -0400

    Bootstrap and handlebars config

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const admin = require('./routes/admin');
-const mongoose = require('mongoose')
-const Mongoose = require('./config/db')
+const mongoose = require('mongoose');
+const Mongoose = require('./config/db');
+const exphbs = require('express-handlebars');
 
 //Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
+//Handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    helpers: {
+        formatDate: (date) => {
+            return moment(date).format('MMM DD YYYY')
+        }
+    }}))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+//Public
+app.use(express.static('public'))
 
 //Routes
 app.use('/admin', admin)

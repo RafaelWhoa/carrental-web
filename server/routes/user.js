96b4ff5bc71b896623e447964ba17e9d87df106b
rafaelwhoa <rafaelwhoa@gmail.com>
2021-08-30 20:36:19 -0400

    Create user register api

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,4 +8,33 @@ router.get('/register', (req, res) => {
     res.render('user/register')
 })
 
+router.post('/api/register', (req, res) => {
+    
+    var errors = []
+
+    if(req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+
+        errors.push({text: "Nome inválido"})
+    }
+
+    if(req.body.email || typeof req.body.email == undefined || req.body.email == null) {
+
+        errors.push({text: "Email inválido"})
+    }
+
+    if(req.body.password || typeof req.body.password == undefined || req.body.password == null) {
+
+        errors.push({text: "Senha inválida"})
+    }
+
+    if(req.body.password != req.body.password2){
+
+        errors.push({text: "Senhas diferentes"})
+    }
+
+    if(errors.length > 0){
+        res.render('user/register', {errors: errors})
+    }
+})
+
 module.exports = router

b38918aad44363203c48e3c8d3989d6bc2bf3cff
Rafael Costa <rafaelwhoa@gmail.com>
2021-09-01 19:01:04 -0400

    Navbar changes and logout method

--- server/routes/user.js
+++ server/routes/user.js
@@ -86,4 +86,10 @@ router.post('/api/login', (req, res, next) => {
     })(req, res, next)
 })
 
+router.get('/logout', (req, res) => {
+    req.logout()
+    req.flash('success_msg', 'Deslogado com sucesso')
+    res.redirect('/')
+})
+
 module.exports = router
